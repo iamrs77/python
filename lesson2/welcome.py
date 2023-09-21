c1edbc2c848e7bd6881f9e8ebc7208d00af20541
Rohit Sharma <iamrs2207@gmail.com>
2023-09-21 00:58:53 -0400

    lesson 2

---
+++ lesson2/welcome.py
@@ -0,0 +1,14 @@
+line01 = '********************'
+line02 = '*                  *'
+line03 = '*     WELCOME!     *'
+
+# print statements
+
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+# comments
